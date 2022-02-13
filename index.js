4ae704234a2d75fe6e80727975a61abb3a15949a
Albert Schotschneider <schotschneider@fzi.de>
2022-02-13 02:05:08 +0100

    Update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = function SalchySummonerMaster(script) {	
+module.exports = function SalchySummonerMaster(script) {
 	///
 	const path = require('path');
 	const fs = require('fs');
 	let config = {};
-	let settingTimeout = null;	
+	let settingTimeout = null;
 	let npcId;
 	let zoned = false;
 	let type;
@@ -17,8 +17,8 @@ module.exports = function SalchySummonerMaster(script) {
 
 	function settingUpdate() {
 		clearTimeout(settingTimeout);
-		settingTimeout = setTimeout(settingSave,1000);
-	}	
+		settingTimeout = setTimeout(settingSave, 1000);
+	}
 	function settingSave() {
 		fs.writeFile(path.join(__dirname, 'config.json'), JSON.stringify(config, undefined, '\t'), err => {
 		});
@@ -78,8 +78,8 @@ module.exports = function SalchySummonerMaster(script) {
 	if (!("sp_id" in config)) {
 		config.sp_id = 3518437209205338;
 		settingUpdate();
-	}	
-	
+	}
+
 	let summons = [
 		{
 			"name": "bank",
@@ -92,7 +92,7 @@ module.exports = function SalchySummonerMaster(script) {
 			"type": 9,
 			"npcId": merchant_id,
 			"value": 70310
-		},		
+		},
 		{
 			"name": "vg",
 			"type": 49,
@@ -100,7 +100,7 @@ module.exports = function SalchySummonerMaster(script) {
 			"value": 609
 		},
 		{
-			"name": "sp",
+			"name": "spp",
 			"type": 9,
 			"npcId": sp_id,
 			"value": 250
@@ -108,72 +108,90 @@ module.exports = function SalchySummonerMaster(script) {
 		{
 			"name": "broker",
 		}
-		
+
 	]
-	
-    script.command.add("sum", (arg) => {
-        if (arg && arg.length > 0) arg = arg.toLowerCase();
-        if (arg) {
-			const summon = getSum(arg);
-            if (!summon) {
-                script.command.message(`Invalid argument idiot`);
-                return;
-            }
-			if(summon && summon.name!="broker") {
-				let buffer = Buffer.alloc(4);
-				buffer.writeUInt32LE(Number(summon.value));
-				script.send("C_REQUEST_CONTRACT", 50, {
-					ContractType: summon.type,
-					NpcCreatureId: summon.npcId,
-					ValueParam: Number(summon.value),
-					ContractRequestee: "",
-					Param: buffer
-				});	
-			} else {
-				script.toClient('S_NPC_MENU_SELECT', 1, {type:28});
-			}				
 
-        } else {
-            sumMenu();
-        }
-    });
-	
-    const gui = {
-        parse(array, title, d = '') {
-            for (let i = 0; i < array.length; i++) {
-                if (d.length >= 16000) {
-                    d += `Gui data limit exceeded, some values may be missing.`;
-                    break;
-                }
-                if (array[i].command) d += `<a href="admincommand:/@${array[i].command}">${array[i].text}</a>`;
-                else if (!array[i].command) d += `${array[i].text}`;
-                else continue;
-            }
-            script.toClient('S_ANNOUNCE_UPDATE_NOTIFICATION', 1, {
-                id: 0,
-                title: title,
-                body: d,
-            });
-        },
-    };	
+	for (let i = 0; i < summons.length - 1; i++) {
+		script.command.add(summons[i].name, () => {
+			let buffer = Buffer.alloc(4);
+			buffer.writeUInt32LE(Number(summons[i].value));
+			script.send('C_REQUEST_CONTRACT', 50, {
+				ContractType: summons[i].type,
+				NpcCreatureId: summons[i].npcId,
+				ValueParam: Number(summons[i].value),
+				ContractRequestee: "",
+				Param: buffer
+			});
+		});
+	}
 
-    function sumMenu() {
-        if (Object.keys(summons).length > 0) {
-            let list = [];
-            summons.forEach((x) => {
-                list.push({
-                    text: `<font size="+20">* ${x.name} </font><br>`,
-                    command: `sum ${x.name}`,
-                });
-            });
-            gui.parse(list, `<font color="#E0B0FF">Summoner Menu</font>`);
-            list = [];
-        }
-    }
+	script.command.add("broker", () => {
+		script.toClient('S_NPC_MENU_SELECT', 1, { type: 28 });
+	});
 
-    function getSum(arg) {
-        return summons.find((e) => e.name.toLowerCase().includes(arg));
-    }	
+	// script.command.add("sum", (arg) => {
+	// 	if (arg && arg.length > 0) arg = arg.toLowerCase();
+	// 	if (arg) {
+	// 		const summon = getSum(arg);
+	// 		if (!summon) {
+	// 			script.command.message(`Invalid argument idiot`);
+	// 			return;
+	// 		}
+	// 		if (summon && summon.name != "broker") {
+	// 			let buffer = Buffer.alloc(4);
+	// 			buffer.writeUInt32LE(Number(summon.value));
+	// 			script.send("C_REQUEST_CONTRACT", 50, {
+	// 				ContractType: summon.type,
+	// 				NpcCreatureId: summon.npcId,
+	// 				ValueParam: Number(summon.value),
+	// 				ContractRequestee: "",
+	// 				Param: buffer
+	// 			});
+	// 		} else {
+	// 			script.toClient('S_NPC_MENU_SELECT', 1, { type: 28 });
+	// 		}
+
+	// 	} else {
+	// 		sumMenu();
+	// 	}
+	// });
+
+	const gui = {
+		parse(array, title, d = '') {
+			for (let i = 0; i < array.length; i++) {
+				if (d.length >= 16000) {
+					d += `Gui data limit exceeded, some values may be missing.`;
+					break;
+				}
+				if (array[i].command) d += `<a href="admincommand:/@${array[i].command}">${array[i].text}</a>`;
+				else if (!array[i].command) d += `${array[i].text}`;
+				else continue;
+			}
+			script.toClient('S_ANNOUNCE_UPDATE_NOTIFICATION', 1, {
+				id: 0,
+				title: title,
+				body: d,
+			});
+		},
+	};
+
+	function sumMenu() {
+		if (Object.keys(summons).length > 0) {
+			let list = [];
+			summons.forEach((x) => {
+				list.push({
+					text: `<font size="+20">* ${x.name} </font><br>`,
+					command: `sum ${x.name}`,
+				});
+			});
+			gui.parse(list, `<font color="#E0B0FF">Summoner Menu</font>`);
+			list = [];
+		}
+	}
+
+	function getSum(arg) {
+		return summons.find((e) => e.name.toLowerCase().includes(arg));
+	}
 
 	script.hook('S_LOAD_TOPO', 3, packet => {
 
@@ -184,35 +202,35 @@ module.exports = function SalchySummonerMaster(script) {
 			zoned = false;
 		}
 	});
-	script.hook('S_SPAWN_NPC', 12, (packet) => {	
+	script.hook('S_SPAWN_NPC', 12, (packet) => {
 		if (zoned == true) {
 			switch (packet.templateId) {
-			case merchant_template:
-				merchant_id = packet.gameId;
-				config.merchant_id = Number(merchant_id);
-				summons[1].npcId = merchant_id
-				settingUpdate();				
-				console.log("Merchant NPC identified, config updated: ", merchant_id);
-				break;
-			case vg_template:
-				vg_id = packet.gameId;
-				config.vg_id = Number(vg_id);
-				summons[2].npcId = vg_id
-				settingUpdate();				
-				console.log("Merchant NPC identified, config updated: ", merchant_id);				
-				console.log("VG NPC identified: ", vg_id);
-				break;	
-			case sp_template:
-				sp_id = packet.gameId;
-				config.sp_id = Number(sp_id);
-				summons[3].npcId = sp_id
-				settingUpdate();				
-				console.log("SP NPC identified, config updated: ", sp_id);
-				break;				
-			default:
-				break;
+				case merchant_template:
+					merchant_id = packet.gameId;
+					config.merchant_id = Number(merchant_id);
+					summons[1].npcId = merchant_id
+					settingUpdate();
+					console.log("Merchant NPC identified, config updated: ", merchant_id);
+					break;
+				case vg_template:
+					vg_id = packet.gameId;
+					config.vg_id = Number(vg_id);
+					summons[2].npcId = vg_id
+					settingUpdate();
+					console.log("Merchant NPC identified, config updated: ", merchant_id);
+					console.log("VG NPC identified: ", vg_id);
+					break;
+				case sp_template:
+					sp_id = packet.gameId;
+					config.sp_id = Number(sp_id);
+					summons[3].npcId = sp_id
+					settingUpdate();
+					console.log("SP NPC identified, config updated: ", sp_id);
+					break;
+				default:
+					break;
 			}
 		}
-	});			
-	
+	});
+
 }
